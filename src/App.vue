68a4d0fdad103c79b604665e1dfd64b4d36d02fd
stefanopriolo <stefanojpriolo@gmail.com>
2023-08-28 17:22:26 +0200

    start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+
+<script>
+import axios from 'axios';
+import TheHeader from './components/TheHeader.vue'
+export default {
+  components: {
+    TheHeader,
+    axios
+  },
+}
+</script>
+
+<template>
+</template>
+
+<style>
+</style>
